a478fe5efd4c8e1f4770a1ce8f1b0a2bb7a41329
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-06 00:14:22 +0200

    Fetch animal data from API

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,14 +1,19 @@
-import json
+import requests
+
+API_KEY = "a30Bxc2MCQw3ON4KGYXSXRGeN8W7JIlcDkElvBKU"
 
 
-def load_data(file_path):
-    """ Loads a JSON file """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+def fetch_data(animal_name):
+    """Fetches animal data from the API Ninjas Animals API."""
+    api_url = f"https://api.api-ninjas.com/v1/animals?name={animal_name}"
+    headers = {"X-Api-Key": API_KEY}
+    response = requests.get(api_url, headers=headers)
+    return response.json()
 
 
 # Load animal data
-animals_data = load_data('animals_data.json')
+animal_name = input("Enter a name of an animal: ")
+animals_data = fetch_data(animal_name)
 
 
 def serialize_animal(animal):
